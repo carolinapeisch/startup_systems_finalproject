9ec18a0c17cb97087927d58fbb85bb345e3ef6ec
root <root@ip-172-31-29-81.ec2.internal>
2016-11-27 16:11:51 +0000

    First commit

---
+++ var/www/app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, abort
+import os
+import subprocess
+import urllib
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Visit /api/num_colors?src=SOMEURL.'
+
+@app.route('/api/num_colors', methods=['GET'])
+def numColors():
+    img = request.args.get('src')
+    if img is None or img == "":
+        abort(500)
+    else:
+        try:
+            filename = img.split('/')[-1]             
+            local = "imgs/"+filename             
+            urllib.request.urlretrieve(img, local)             
+            colors = subprocess.run(["identify", "-format", "%k", local], stdout=subprocess.PIPE)
+
+            if(colors.returncode == 0):
+                return colors.stdout
+            else:
+                abort(500)
+        except:
+            abort(500)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
